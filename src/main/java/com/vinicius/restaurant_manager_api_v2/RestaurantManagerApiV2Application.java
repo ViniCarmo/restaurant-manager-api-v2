bf79f7a326a4bd63a2bac55c41d78651f5bf3eee
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-07-07 17:19:33 -0300

    Create Project

---
+++ src/main/java/com/vinicius/restaurant_manager_api_v2/RestaurantManagerApiV2Application.java
@@ -0,0 +1,13 @@
+package com.vinicius.restaurant_manager_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagerApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagerApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/restaurant_manager_api_v2/RestaurantManagerApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.restaurant_manager_api_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
